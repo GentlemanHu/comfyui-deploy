b2b82031de3391c2a880c150968a73d18b43d9a4
Gentleman.Hu <justfeelingme@gmail.com>
2026-05-13 21:43:21 +0800

    Build local web image with GitHub Actions

--- web/next.config.mjs
+++ web/next.config.mjs
@@ -15,6 +15,7 @@ const withMDX = nextMDX({
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   pageExtensions: ["js", "jsx", "ts", "tsx", "mdx"],
+  output: "standalone",
   eslint: {
     ignoreDuringBuilds: true,
   },
